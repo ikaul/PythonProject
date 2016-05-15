9a933407580757d69d2cdd49f069072ebe7ae5dd
Ishan Kaul <ikaul@pulsesecure.net>
2016-05-15 15:16:44 -0700

    Added TestCases to PalindromePermutations

--- String_and_Arrays/palindromePermutaion.py
+++ String_and_Arrays/palindromePermutaion.py
@@ -1,31 +1,44 @@
 #!/usr/bin/python
+import unittest
+
 #Return permutaion of strings which are palindromes
 
-def palindromePermutation(str):
-	strArr = list(str)
-	l = len(strArr)
-	if l % 2 == 0 and getOddCharCount(str) == 0:
+def palindromePermutation(string):
+	if not isinstance(string, str):
+		return "Invalid Input"
+	stringArr = list(string)
+	l = len(stringArr)
+	if l % 2 == 0 and getOddCharCount(string) == 0:
 		return 1
-	elif l % 2 == 1 and getOddCharCount(str) == 1:
+	elif l % 2 == 1 and getOddCharCount(string) == 1:
 		return 1
 	else:
 		return 0
 
-def getOddCharCount(str):
-	strArr = list(str)
-	strCount = dict()
-	for x in strArr:
-		if strCount.has_key(x):
-			strCount[x] += 1
+def getOddCharCount(string):
+	stringArr = list(string)
+	stringCount = dict()
+	for x in stringArr:
+		if stringCount.has_key(x):
+			stringCount[x] += 1
 		else:
-			strCount[x] = 1
+			stringCount[x] = 1
 	count = 0
-	for x in strCount.keys():
-		if strCount[x] % 2 == 1:
+	for x in stringCount.keys():
+		if stringCount[x] % 2 == 1:
 			count += 1
-	return count	
-	return 1
+	return count
 
-print palindromePermutation("test")	
-print palindromePermutation("e tet")	
-print palindromePermutation("abcd abc")	
+class TestPalindromes( unittest.TestCase ):
+	def test_validInput(self):
+		self.assertFalse( palindromePermutation("test") )
+		self.assertTrue( palindromePermutation("e tet") )
+		self.assertFalse( palindromePermutation("abcd abc") )
+	def test_boundaryInput(self):
+		self.assertTrue( palindromePermutation("a") )
+		self.assertTrue( palindromePermutation("") )
+	def test_invalidInput(self):
+		self.assertEqual( "Invalid Input", palindromePermutation(1) )
+		self.assertEqual( "Invalid Input", palindromePermutation(None) )
+
+unittest.main()
