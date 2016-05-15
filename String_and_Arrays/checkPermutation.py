087903180a6096499a63994153357db437f7090e
Ishan Kaul <ikaul@pulsesecure.net>
2016-05-15 14:36:43 -0700

    Added Unit Tests to checkPermutation

--- String_and_Arrays/checkPermutation.py
+++ String_and_Arrays/checkPermutation.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
+import unittest
+
 #Given 2 str, check if they are permutaion of each other
 def checkPermutation(str1,str2):
+	if not isinstance(str1, str) or not isinstance(str2, str):
+		return "Invalid Input"
+
 	str1Arr = list(str1)
 	str2Arr = list(str2)
 	#They need to have same #chars
@@ -22,7 +27,19 @@ def checkPermutation(str1,str2):
 			return 0 
 	return 1 
 
-print checkPermutation("test", "etst")
-print checkPermutation("best", "etst")
-print checkPermutation("ta", "at")
-print checkPermutation("bbbbbbbeeeeeestt", "etstbbbbbbbeeeee")
+class TestPermutations(unittest.TestCase):
+	def test_validInput(self):
+		self.assertTrue(checkPermutation("test", "etst"))
+		self.assertFalse(checkPermutation("best", "etst"))
+		self.assertTrue(checkPermutation("ta", "at"))
+		self.assertTrue(checkPermutation("bbbbbbbeeeeeestt", "etstbbbbbbbeeeee"))
+	def test_boundaryInput(self):
+		self.assertTrue(checkPermutation("t", "t"))
+		self.assertFalse(checkPermutation("x", "y"))
+	def test_invalidInput(self):
+		self.assertEqual(checkPermutation(-1, 1), "Invalid Input")
+		self.assertEqual(checkPermutation(-1.0, 1), "Invalid Input")
+		self.assertEqual(checkPermutation("test", 1), "Invalid Input")
+		self.assertEqual(checkPermutation("test", None), "Invalid Input")
+if __name__ == '__main__':
+    unittest.main()
