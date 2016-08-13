609495109adca1e56b9747d2ddf9a9d656aa98ba
Ishan Kaul <kaulishan2007@gmail.com>
2016-08-13 14:42:14 -0700

    Added Unit tests to isUnique function

--- String_and_Arrays/isUnique.py
+++ String_and_Arrays/isUnique.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python
+import unittest
+
 #Check if string has all unique characters
 def isUnique(str):
+	if str == None:
+		return None
 	if len(str) < 2:
-		return 1
+		return True
 	strArr = list(str)
 	if len(list(set(strArr))) == len(strArr):
-		return 1
+		return True
 	else:
-		return 0
+		return False
 
-print isUnique("test")
-print isUnique("ABCDEFGH")
-print isUnique("")
-print isUnique("AaBCDEFGHijklmpoqw")
+class TestisUnique(unittest.TestCase):
+	def setUp(self):
+		self.str1 = "test"
+		self.str2 = "ABCDEFGH"
+		self.str3 = "AaBCDEFGHijklmpoqw"
+	def test_validInput(self):
+		self.assertFalse(isUnique(self.str1))
+		self.assertTrue(isUnique(self.str2))
+		self.assertTrue(isUnique(self.str3))
+		self.assertTrue(isUnique(""))
+		self.assertTrue(isUnique("A"))
+		self.assertFalse(isUnique("AA"))
+	def test_invalidInput(self):
+		self.assertEqual(None, isUnique(None))
+
+if __name__ == '__main__':
+    unittest.main()
